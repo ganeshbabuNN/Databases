b4b9f9ffe0afba0a75d8be9d1960920df2e5fd5c
Ganesh Babu <ganeshbabu346@gmail.com>
2015-11-12 09:01:01 +0530

    ..

--- RDMS/Oracle Database/Function/Function_Nested_functions.sql
+++ RDMS/Oracle Database/Function/Function_Nested_functions.sql
@@ -1,6 +1,21 @@
+---let us take the for loop scenarios
 DECLARE
   v_increment NUMBER := 3;
+BEGIN
+  FOR I IN REVERSE 1..10
+  LOOP
+    IF mod(i,v_increment) != 0
+    THEN
+      CONTINUE;
+    END IF;
+    dbms_output.put_line(i);
+  END LOOP;
+END;
+/
 
+--now let us modularize the logic by nested functions
+DECLARE
+  v_increment NUMBER := 3;
   FUNCTION modit(
     p_dividend IN NUMBER,
     p_divisior IN NUMBER )
@@ -9,9 +24,7 @@ DECLARE
   BEGIN
     RETURN (mod(p_dividend,p_divisior) = 0);
   END;
-
 BEGIN
-
   FOR I IN REVERSE 1..10
   LOOP
     IF modit(i,v_increment)
