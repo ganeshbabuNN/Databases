fc45b242885297efd9c4d2814f435a3c64b9f254
Ganesh Babu <ganeshbabu346@gmail.com>
2015-12-06 02:47:42 +0530

    ..

--- RDMS/Oracle Database/Triggers/Trigger_InsteadOfTrigger_1.sql
+++ RDMS/Oracle Database/Triggers/Trigger_InsteadOfTrigger_1.sql
@@ -1,3 +1,17 @@
+
+
+---create a view
+CREATE OR REPLACE VIEW hr_emps_by_dept
+AS
+  SELECT first_name, 
+         last_name, 
+         department_id, 
+         count(*) over (partition by department_id) dept_cnt
+    FROM employees;
+    
+select * from hr_emps_by_dept where last_name='Grant';
+
+
 Ensure there is a trigger on the employee table.
 
 Trigger_CompundTrigger.sql
